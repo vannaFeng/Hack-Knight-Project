21f2754a1cbdf690122403ac1ce1789d96db8baf
unknown <lincalvin76@gmail.com>
2025-10-18 18:18:05 -0400

    Sorted AI Response into a table, Add new row section created, Animation for loading text and table dropping in created, Allergy input + submit button created, Allergy added onto object at the end

--- what-the-fridge/src/App.jsx
+++ what-the-fridge/src/App.jsx
@@ -1,5 +1,5 @@
 import './style.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
@@ -11,6 +11,25 @@ function App() {
 
   const [resultData, setResultData] = useState(null);
   const [editingItem, setEditingItem] = useState(null);
+  const [allergyInput, setAllergyInput] = useState('');
+  const [sentResults, setSentResults] = useState(false);
+  const [page, setPage] = useState(0);
+  const itemsPerPage = 5;
+
+  const [dots, setDots] = useState('');
+
+  useEffect(() => {
+    if (!countLoad) {
+      setDots('');
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setDots(prev => (prev.length >= 3 ? '' : prev + '.'));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [countLoad]);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -64,12 +83,21 @@ const handleFormSubmission = async (e) => {
     }
 
     if (response.ok) {
+
+      if (parsedData.Invalid === 0) {
+        alert("No food detected in the image.");
+        setCountLoad(false);
+        return;
+      }
+
       setResultData(parsedData);
       console.log(parsedData);
       setImage2(URL.createObjectURL(file));
       setImage(null);
+      setPage(0);
     } else {
       console.error(data.error || 'Something went wrong');
+      alert(data.error || 'Something went wrong')
     }
   } catch (err) {
     console.error('Network error:', err);
@@ -77,6 +105,32 @@ const handleFormSubmission = async (e) => {
     setCountLoad(false);
   }
 };
+
+  const itemsArray = Object.entries(resultData || {});
+  const totalPages = Math.ceil(itemsArray.length / itemsPerPage) || 1;
+  const [newItemName, setNewItemName] = useState('');
+  const [newItemQuantity, setNewItemQuantity] = useState(0);
+
+  const visibleItems = itemsArray.slice(
+    page * itemsPerPage,
+    page * itemsPerPage + itemsPerPage
+  );
+
+  const addNewRow = () => {
+    if (!resultData) return;
+
+    let counter = 1;
+    let newKey  = `New Item ${counter}`;
+    while (resultData[newKey]) {
+      counter += 1;
+      newKey = `New Item ${counter}`;
+    }
+
+    setResultData(prevData => ({
+      ...prevData,
+      [newKey]: {Quantity: 0}
+    }));
+  };
 
   return (
     <div className="bg-[#0e0e0e] h-screen w-screen flex flex-row">
@@ -166,15 +220,15 @@ const handleFormSubmission = async (e) => {
       </div>
       
       {/* Right Half Section */}
-      <div className="absolute inset-0 bg-gradient-radial from-[#3a3a3a] via-[#1a1a1a] to-[#0e0e0e] opacity-90 h-screen w-3/4 pointer-events-none flex left-1/4 justify-center align-center font-titillium">
+      <div className="absolute inset-0 bg-gradient-radial from-[#3a3a3a] via-[#1a1a1a] to-[#0e0e0e] opacity-90 h-screen w-3/4 flex left-1/4 justify-center align-center font-titillium">
           
-          {countLoad && (
-            <p className="text-white text-3xl mt-5">Counting Items...</p>
+          {countLoad && !sentResults && (
+            <p className="text-white text-3xl mt-5 text-center">Counting Items{dots}</p>
           )}
 
-          {!countLoad && resultData && (
+          {!countLoad && resultData && !sentResults && (
             <div className="w-full max-w-2xl text-white">
-              <p className="text-3xl mb-4 mt-5">Finished Counting!</p>
+              <p className="text-3xl mb-4 mt-5 text-center">Finished Counting!</p>
               <table className="w-full text-left border-collapse">
                 <thead>
                   <tr>
@@ -183,11 +237,11 @@ const handleFormSubmission = async (e) => {
                   </tr>
                 </thead>
                 <tbody>
-                  {Object.entries(resultData).map(([item, info]) => (
-                    <tr key={item}>
-                      <td className="border-b border-gray-600 p-2">{item}</td>
+                  {visibleItems.map(([item, info]) => (
+                    <tr key={item} className={`border-b border-gray-600 p-2 drop-animation`}>
+                      <td className="p-2">{item}</td>
                       <td
-                        className="border-b border-gray-600 p-2 cursor-pointer"
+                        className="p-2 cursor-pointer"
                         onClick={() => setEditingItem(item)}
                       >
                         {editingItem === item ? (
@@ -197,7 +251,10 @@ const handleFormSubmission = async (e) => {
                             value={info.Quantity}
                             autoFocus
                             onChange={(e) => {
-                              const newQuantity = parseInt(e.target.value, 10) || 0;
+                              let newQuantity = parseInt(e.target.value, 10) || 0;
+                              if (newQuantity < 0) {
+                                newQuantity = 0;
+                              }
                               setResultData(prevData => ({
                                 ...prevData,
                                 [item]: {
@@ -217,18 +274,107 @@ const handleFormSubmission = async (e) => {
                       </td>
                     </tr>
                   ))}
+                  
+                  {/* Add Row Section */}
+                  {page === totalPages - 1 && (
+                    <tr>
+                      <td className="border-b border-gray-600 p-2">
+                        <input
+                          type="text"
+                          placeholder="New Item"
+                          className="w-full bg-gray-700 text-white p-1 rounded"
+                          value={newItemName}
+                          onChange={(e) => setNewItemName(e.target.value)}
+                        />
+                      </td>
+                      <td className="border-b border-gray-600 p-2">
+                        <input
+                          type="number"
+                          className="w-16 bg-gray-700 text-white p-1 rounded"
+                          value={newItemQuantity}
+                          onChange={(e) => setNewItemQuantity(parseInt(e.target.value, 10) || 0)}
+                        />
+                        <button
+                          className="ml-2 px-2 py-1 bg-green-600 rounded hover:bg-green-500 text-white"
+                          onClick={() => {
+                            if (!newItemName) return;
+                            setResultData(prev => ({
+                              ...prev,
+                              [newItemName]: { Quantity: newItemQuantity }
+                            }));
+                            setNewItemName('');
+                            setNewItemQuantity(0);
+                          }}
+                        >
+                          Add
+                        </button>
+                      </td>
+                    </tr>
+                    )}
                 </tbody>
               </table>
+
+              <div className="flex align-center justify-center mt-3">
+                <button
+                  onClick={() => setPage(prev => (prev === 0 ? totalPages - 1 : prev - 1))}
+                  className="px-3 py-1 bg-gray-700 text-white rounded hover:bg-gray-600 mr-5"
+                >
+                  ← 
+                </button>
+
+                <span className="text-white">
+                  Page {page + 1} of {totalPages}
+                </span>
+
+                <button
+                  onClick={() => setPage(prev => (prev === totalPages - 1 ? 0 : prev + 1))}
+                  className="px-3 py-1 bg-gray-700 text-white rounded hover:bg-gray-600 ml-5"
+                >
+                  →
+                </button>
+              </div>
+
+              <div className = "flex flex-col items-center justify-center mt-20">
+                <input
+                  type="text"
+                  placeholder="Enter any allergies you may have"
+                  className="w-full bg-gray-700 text-white p-1 rounded"
+                  value={allergyInput}
+                  onChange={(e) => setAllergyInput(e.target.value)}
+                >
+                </input>
+
+                <button 
+                  onClick={() =>{
+                    if (!resultData) return;
+                    
+                    setResultData(prevData => {
+                      const updatedData = {
+                        ...prevData,
+                        Allergies: allergyInput
+                      };
+                      console.log("Updated Data:", updatedData);
+                      return updatedData;
+                    });
+                    
+                    setSentResults(true);
+                  }}
+                  className="px-4 py-2 w-1/4 bg-green-400 text-white rounded-lg hover:bg-green-600 font-titillium hover:scale-110 transition-all duration-300 mt-5"
+                >
+                  Search for Recipes
+                </button>
+              </div>
+
             </div>
           )}
 
-          {!countLoad && !resultData && (
-            <p className="text-white text-3xl mt-5">No items counted yet.</p>
+          {!countLoad && !resultData && !sentResults && (
+            <p className="text-white text-3xl mt-5 text-center">No items counted yet.</p>
           )}
 
+
+
       </div>
-
-
     </div>
   )
 }
